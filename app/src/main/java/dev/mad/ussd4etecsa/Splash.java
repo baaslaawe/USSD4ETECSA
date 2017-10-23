a0892ef9cb46c9657083428f4c6dfee70d1415e1
Daymel <daym3l@nauta.cu>
2017-10-23 09:36:23 -0400

    Se agrega la funcionalidad de permisos multiples

--- app/src/main/java/dev/mad/ussd4etecsa/Splash.java
+++ app/src/main/java/dev/mad/ussd4etecsa/Splash.java
@@ -9,24 +9,25 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Splash extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1;
-    private static final int MY_PERMISSIONS_REQUEST_ACCESS_NET_STATE = 2;
-    private static final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 0;
-    private static final int MY_PERMISSIONS_REQUEST_READ_SMS = 3;
+    public static final int REQUEST_MULTIPLE_PERMISSIONS_ID = 456;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
         checkPermiso();
         Thread myThread = new Thread() {
             @Override
             public void run() {
                 try {
-                    sleep(2000);
+                    sleep(3000);
 
                     Intent intent = new Intent(getApplicationContext(), Principal.class);
                     startActivity(intent);
@@ -38,113 +39,44 @@ public class Splash extends AppCompatActivity {
         };
         myThread.start();
 
+
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_CALL_PHONE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                } else {
-
-                }
-                return;
-            }
-            case MY_PERMISSIONS_REQUEST_ACCESS_NET_STATE: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                } else {
-
-                }
-                return;
-            }
-            case MY_PERMISSIONS_REQUEST_READ_PHONE_STATE: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                } else {
-
-                }
-                return;
-            }
-            case MY_PERMISSIONS_REQUEST_READ_SMS: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                } else {
-
-                }
-                return;
-            }
-
-
-        }
-    }
     /**
      * Chequea los premisos necesarios para aplicacion
      */
-    public void checkPermiso() {
+    public boolean checkPermiso() {
 
+        boolean flag = true;
         int permisoCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
         int permisoCheck2 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE);
         int permisoCheck3 = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
         int permisoCheck4 = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS);
 
+        List<String> lista_permisos = new ArrayList<>();
+
+
         if (permisoCheck != PackageManager.PERMISSION_GRANTED) {
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)) {
-
-
-            } else {
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.CALL_PHONE},
-                        MY_PERMISSIONS_REQUEST_CALL_PHONE);
-
-            }
+            lista_permisos.add(Manifest.permission.CALL_PHONE);
         }
 
         if (permisoCheck3 != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE)) {
-
-
-            } else {
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_PHONE_STATE},
-                        MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
-
-            }
+            lista_permisos.add(Manifest.permission.READ_PHONE_STATE);
         }
         if (permisoCheck2 != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_NETWORK_STATE)) {
-
-
-            } else {
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_NETWORK_STATE},
-                        MY_PERMISSIONS_REQUEST_ACCESS_NET_STATE);
-
-            }
+            lista_permisos.add(Manifest.permission.ACCESS_NETWORK_STATE);
         }
 
         if (permisoCheck4 != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_SMS)) {
-
-
-            } else {
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_SMS},
-                        MY_PERMISSIONS_REQUEST_READ_SMS);
-
-            }
+            lista_permisos.add(Manifest.permission.READ_SMS);
         }
+        if (!lista_permisos.isEmpty()) {
+            ActivityCompat.requestPermissions(this, lista_permisos.toArray(new String[lista_permisos.size()]), REQUEST_MULTIPLE_PERMISSIONS_ID);
+            flag = false;
+        }
+        return flag;
 
     }
+
+
 }
